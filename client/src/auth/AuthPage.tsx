5ea83b8aca869915ebe188b02587147a104e2032
Aaron Castells <acastells@captio.com>
2021-04-03 19:12:06 +0200

    Lint && prettiers

--- client/src/auth/AuthPage.tsx
+++ client/src/auth/AuthPage.tsx
@@ -1,65 +1,64 @@
-import React, {useState} from "react";
-import './Auth.css';
-import Form from "../form/Form";
+import React, { useState } from 'react'
+import './Auth.css'
+import Form from '../form/Form'
 
 function AuthPage() {
-
-    const submitText = 'Login'
-    const maxLogLenght = 5
-    const [serverData, setServerData] = useState<serverData[]>([])
-    const handleServerData = (response: any) => {
-        const date = new Date()
-        const data: serverData = {
-            login: response.data.login,
-            password: response.data.password,
-            status: response.status,
-            date: `${date.getFullYear()}/${date.getMonth()}/${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-        }
-        setServerData(arr => [data, ...arr.slice(0, maxLogLenght)]);
-        console.log(response)
+  const submitText = 'Login'
+  const maxLogLenght = 5
+  const [authData, setAuthData] = useState<AuthData[]>([])
+  const handleServerData = (response: any) => {
+    const date = new Date()
+    const data: AuthData = {
+      login: response.data.login,
+      password: response.data.password,
+      status: response.status,
+      date: `${date.getFullYear()}/${date.getMonth()}/${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
     }
+    setAuthData((arr) => [data, ...arr.slice(0, maxLogLenght)])
+    console.log(response)
+  }
 
-    return (
-        <div>
-            <div className={'container'}>
-                <Form id={'login-form'} submitText={submitText} responseFromServer={handleServerData} />
-            </div>
-            <div className={'container'}>
-                <div className={'card server-data'}>
-                    {
-                        serverData.map((data, i) => {
-                            return (
-                            <div key={i}>
-                                <LogItem status={data.status} login={data.login} password={data.password} datetime={data.date}/>
-                            </div>
-                            )}
-                            )
-                    }
-                </div>
-            </div>
+  return (
+    <div>
+      <div className={'container'}>
+        <Form id={'login-form'} submitText={submitText} responseFromServer={handleServerData} />
+      </div>
+      <div className={'container'}>
+        <div className={'card server-data'}>
+          {authData.map((data, i) => {
+            return (
+              <div key={i}>
+                <LogItem
+                  status={data.status}
+                  login={data.login}
+                  password={data.password}
+                  datetime={data.date}
+                />
+              </div>
+            )
+          })}
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-export default AuthPage;
+export default AuthPage
 
-
-interface serverData {
-    date: string;
-    status: number;
-    login: string;
-    password: string;
+interface AuthData {
+  date: string
+  status: number
+  login: string
+  password: string
 }
 
-
-function LogItem (props: any) {
-
-    return (
-        <div className={'info-row'}>
-            <span>{props.datetime}</span>
-            <span>{props.status}</span>
-            <span>{props.login}</span>
-            <span>{props.password}</span>
-        </div>
-    )
+function LogItem(props: any) {
+  return (
+    <div className={'info-row'}>
+      <span>{props.datetime}</span>
+      <span>{props.status}</span>
+      <span>{props.login}</span>
+      <span>{props.password}</span>
+    </div>
+  )
 }
--- client/src/form/Form.tsx
+++ client/src/form/Form.tsx
@@ -1,64 +1,78 @@
-import axios from "axios";
-import {AuthModel} from "../auth/auth.model";
-import {useState} from "react";
+import axios from 'axios'
+import { AuthModel } from '../auth/auth.model'
+import { useState } from 'react'
 
 const Form = (props: any) => {
+  const [login, setLogin] = useState('')
 
-    const [login, setLogin] = useState(
-        ''
-    )
+  const [password, setPassword] = useState('')
 
-    const [password, setPassword] = useState(
-        ''
-    )
-
-    const submit = (event: any) => {
-        event.preventDefault()
-        let authUser: AuthModel = {
-            login: login,
-            password: password
-        }
-
-        let response = axios.post('http://localhost:3001/api/auth', authUser)
-
-        response.then((res => {
-            props.responseFromServer(res);
-        })).catch((err => {
-            props.responseFromServer(err.response)
-        }))
+  const submit = (event: any) => {
+    event.preventDefault()
+    const authUser: AuthModel = {
+      login,
+      password,
     }
 
-    const handleLoginChange = (event: any) => {
-        setLogin(event.target.value)
-    }
+    const response = axios.post('http://localhost:3001/api/auth', authUser)
 
-    const handlePasswordChange = (event: any) => {
-        setPassword(event.target.value)
-    }
+    response
+      .then((res) => {
+        props.responseFromServer(res)
+      })
+      .catch((err) => {
+        props.responseFromServer(err.response)
+      })
+  }
 
+  const handleLoginChange = (event: any) => {
+    setLogin(event.target.value)
+  }
 
-    return(
-        <div className={'card'}>
-            <form key={props.id} onSubmit={submit}>
-                <div className={'card-content'}>
-                    <input value={login} className={'input'} type={'text'} name={'login'} key={'login'} placeholder={'Enter your login'} onChange={handleLoginChange}/>
-                    <input value={password} className={'input'} type={'password'} name={'password'} key={'password'} placeholder={'Password'} onChange={handlePasswordChange}/>
-                </div>
-                <div className={'card-footer'}>
-                    <div className={'card-footer-item'}>
-                        <button className={'button is-primary'} type={"submit"}>{props.submitText}</button>
-                    </div>
-                </div>
-            </form>
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value)
+  }
+
+  return (
+    <div className={'card'}>
+      <form key={props.id} onSubmit={submit}>
+        <div className={'card-content'}>
+          <input
+            value={login}
+            className={'input'}
+            type={'text'}
+            name={'login'}
+            key={'login'}
+            placeholder={'Enter your login'}
+            onChange={handleLoginChange}
+          />
+          <input
+            value={password}
+            className={'input'}
+            type={'password'}
+            name={'password'}
+            key={'password'}
+            placeholder={'Password'}
+            onChange={handlePasswordChange}
+          />
         </div>
-    )
+        <div className={'card-footer'}>
+          <div className={'card-footer-item'}>
+            <button className={'button is-primary'} type={'submit'}>
+              {props.submitText}
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Form
 
 export interface FormTemplate {
-    id: string;
-    label: string;
-    name: string;
-    type: 'text'|'password'
+  id: string
+  label: string
+  name: string
+  type: 'text' | 'password'
 }
